9a02902a323f04ae1b5415ad606191c5750c406c
Kwan <jky6216@naver.com>
2022-11-10 15:34:39 +0900

    Add MyStupigArenaCharacter class and set diretion

---
+++ Source/StupigArena/Public/MyStupigArenaCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StupigArenaCharacter.h"
+#include "MyStupigArenaCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STUPIGARENA_API AMyStupigArenaCharacter : public AStupigArenaCharacter
+{
+	GENERATED_BODY()
+	
+};
